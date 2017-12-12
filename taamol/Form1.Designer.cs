a4cc1bebb27858cafc7a3c5f7e905f5e6758de4b
parham <pt2902@gmail.com>
2017-12-12 21:59:37 +0330

    Add project files.

---
+++ taamol/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace taamol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblfamily = new System.Windows.Forms.Label();
+            this.lblname = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lbltab1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lbltab2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.lbltab3 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuSeparator1 = new Bunifu.Framework.UI.BunifuSeparator();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(84)))), ((int)(((byte)(186)))));
+            this.panel1.Controls.Add(this.lblfamily);
+            this.panel1.Controls.Add(this.lblname);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(770, 135);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblfamily
+            // 
+            this.lblfamily.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblfamily.Location = new System.Drawing.Point(112, 74);
+            this.lblfamily.Name = "lblfamily";
+            this.lblfamily.Size = new System.Drawing.Size(184, 22);
+            this.lblfamily.TabIndex = 5;
+            this.lblfamily.Text = "family";
+            // 
+            // lblname
+            // 
+            this.lblname.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblname.Location = new System.Drawing.Point(112, 32);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(184, 22);
+            this.lblname.TabIndex = 3;
+            this.lblname.Text = "name";
+            this.lblname.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::taamol.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(41, 213);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(702, 306);
+            this.panel2.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 141);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(770, 410);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // lbltab1
+            // 
+            this.lbltab1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lbltab1.AutoSize = true;
+            this.lbltab1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbltab1.Location = new System.Drawing.Point(62, 173);
+            this.lbltab1.Name = "lbltab1";
+            this.lbltab1.Size = new System.Drawing.Size(34, 17);
+            this.lbltab1.TabIndex = 0;
+            this.lbltab1.Text = "gym";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(636, 143);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lbltab2
+            // 
+            this.lbltab2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lbltab2.AutoSize = true;
+            this.lbltab2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbltab2.Location = new System.Drawing.Point(157, 173);
+            this.lbltab2.Name = "lbltab2";
+            this.lbltab2.Size = new System.Drawing.Size(59, 17);
+            this.lbltab2.TabIndex = 2;
+            this.lbltab2.Text = "member";
+            this.lbltab2.Click += new System.EventHandler(this.bunifuCustomLabel2_Click);
+            // 
+            // lbltab3
+            // 
+            this.lbltab3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lbltab3.AutoSize = true;
+            this.lbltab3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbltab3.Location = new System.Drawing.Point(276, 173);
+            this.lbltab3.Name = "lbltab3";
+            this.lbltab3.Size = new System.Drawing.Size(60, 17);
+            this.lbltab3.TabIndex = 3;
+            this.lbltab3.Text = "financial";
+            // 
+            // bunifuSeparator1
+            // 
+            this.bunifuSeparator1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSeparator1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(118)))), ((int)(((byte)(210)))));
+            this.bunifuSeparator1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(118)))), ((int)(((byte)(210)))));
+            this.bunifuSeparator1.LineThickness = 3;
+            this.bunifuSeparator1.Location = new System.Drawing.Point(54, 194);
+            this.bunifuSeparator1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bunifuSeparator1.Name = "bunifuSeparator1";
+            this.bunifuSeparator1.Size = new System.Drawing.Size(52, 12);
+            this.bunifuSeparator1.TabIndex = 4;
+            this.bunifuSeparator1.Transparency = 255;
+            this.bunifuSeparator1.Vertical = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(770, 545);
+            this.Controls.Add(this.bunifuSeparator1);
+            this.Controls.Add(this.lbltab3);
+            this.Controls.Add(this.lbltab2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lbltab1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private Bunifu.Framework.UI.BunifuCustomLabel lbltab1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Bunifu.Framework.UI.BunifuCustomLabel lbltab2;
+        private Bunifu.Framework.UI.BunifuSeparator bunifuSeparator1;
+        private Bunifu.Framework.UI.BunifuCustomLabel lbltab3;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Label lblfamily;
+    }
+}
+
