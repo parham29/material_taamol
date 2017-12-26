5a8da42a7ab93f7c800e2cf50fe135f9ee8e7e93
parham <pt2902@gmail.com>
2017-12-26 15:11:44 +0330

    kjjhkhk

--- taamol/addmember.Designer.cs
+++ taamol/addmember.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace taamol
 {
-    partial class Form1
+    partial class addmember
     {
         /// <summary>
         /// Required designer variable.
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addmember));
             this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
             this.Btn_login = new Bunifu.Framework.UI.BunifuThinButton2();
             this.Txt_username = new Bunifu.Framework.UI.BunifuMetroTextbox();
--- taamol/addmember.cs
+++ taamol/addmember.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace taamol
 {
-    public partial class Form1 : Form
+    public partial class addmember : Form
     {
-        public Form1()
+        public addmember()
         {
             InitializeComponent();
         }
