6c6fea081dbbd2c7ba13a574ddd94f4146dcbb6f
parham <pt2902@gmail.com>
2017-12-26 15:02:26 +0330

    Form1.cs added

---
+++ taamol/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace taamol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.Btn_login = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.Txt_username = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.bunifuMetroTextbox1 = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.bunifuMetroTextbox2 = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.bunifuCheckbox1 = new Bunifu.Framework.UI.BunifuCheckbox();
+            this.bunifuiOSSwitch1 = new Bunifu.Framework.UI.BunifuiOSSwitch();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // Btn_login
+            // 
+            this.Btn_login.ActiveBorderThickness = 1;
+            this.Btn_login.ActiveCornerRadius = 20;
+            this.Btn_login.ActiveFillColor = System.Drawing.Color.OrangeRed;
+            this.Btn_login.ActiveForecolor = System.Drawing.SystemColors.WindowText;
+            this.Btn_login.ActiveLineColor = System.Drawing.Color.Transparent;
+            this.Btn_login.BackColor = System.Drawing.SystemColors.Control;
+            this.Btn_login.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_login.BackgroundImage")));
+            this.Btn_login.ButtonText = "Login";
+            this.Btn_login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_login.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_login.ForeColor = System.Drawing.Color.SeaGreen;
+            this.Btn_login.IdleBorderThickness = 1;
+            this.Btn_login.IdleCornerRadius = 20;
+            this.Btn_login.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(177)))), ((int)(((byte)(136)))));
+            this.Btn_login.IdleForecolor = System.Drawing.Color.White;
+            this.Btn_login.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(177)))), ((int)(((byte)(136)))));
+            this.Btn_login.Location = new System.Drawing.Point(159, 371);
+            this.Btn_login.Margin = new System.Windows.Forms.Padding(5);
+            this.Btn_login.Name = "Btn_login";
+            this.Btn_login.Size = new System.Drawing.Size(200, 47);
+            this.Btn_login.TabIndex = 1;
+            this.Btn_login.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Btn_login.Click += new System.EventHandler(this.Btn_login_Click);
+            // 
+            // Txt_username
+            // 
+            this.Txt_username.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Txt_username.BorderColorFocused = System.Drawing.Color.OrangeRed;
+            this.Txt_username.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(177)))), ((int)(((byte)(136)))));
+            this.Txt_username.BorderColorMouseHover = System.Drawing.Color.OrangeRed;
+            this.Txt_username.BorderThickness = 1;
+            this.Txt_username.CausesValidation = false;
+            this.Txt_username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Txt_username.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_username.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.Txt_username.isPassword = false;
+            this.Txt_username.Location = new System.Drawing.Point(68, 52);
+            this.Txt_username.Margin = new System.Windows.Forms.Padding(4);
+            this.Txt_username.Name = "Txt_username";
+            this.Txt_username.Size = new System.Drawing.Size(395, 44);
+            this.Txt_username.TabIndex = 2;
+            this.Txt_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // bunifuMetroTextbox1
+            // 
+            this.bunifuMetroTextbox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.bunifuMetroTextbox1.BorderColorFocused = System.Drawing.Color.OrangeRed;
+            this.bunifuMetroTextbox1.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(177)))), ((int)(((byte)(136)))));
+            this.bunifuMetroTextbox1.BorderColorMouseHover = System.Drawing.Color.OrangeRed;
+            this.bunifuMetroTextbox1.BorderThickness = 1;
+            this.bunifuMetroTextbox1.CausesValidation = false;
+            this.bunifuMetroTextbox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.bunifuMetroTextbox1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuMetroTextbox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.bunifuMetroTextbox1.isPassword = false;
+            this.bunifuMetroTextbox1.Location = new System.Drawing.Point(68, 121);
+            this.bunifuMetroTextbox1.Margin = new System.Windows.Forms.Padding(4);
+            this.bunifuMetroTextbox1.Name = "bunifuMetroTextbox1";
+            this.bunifuMetroTextbox1.Size = new System.Drawing.Size(395, 44);
+            this.bunifuMetroTextbox1.TabIndex = 3;
+            this.bunifuMetroTextbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // bunifuMetroTextbox2
+            // 
+            this.bunifuMetroTextbox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.bunifuMetroTextbox2.BorderColorFocused = System.Drawing.Color.OrangeRed;
+            this.bunifuMetroTextbox2.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(177)))), ((int)(((byte)(136)))));
+            this.bunifuMetroTextbox2.BorderColorMouseHover = System.Drawing.Color.OrangeRed;
+            this.bunifuMetroTextbox2.BorderThickness = 1;
+            this.bunifuMetroTextbox2.CausesValidation = false;
+            this.bunifuMetroTextbox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.bunifuMetroTextbox2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuMetroTextbox2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.bunifuMetroTextbox2.isPassword = false;
+            this.bunifuMetroTextbox2.Location = new System.Drawing.Point(68, 199);
+            this.bunifuMetroTextbox2.Margin = new System.Windows.Forms.Padding(4);
+            this.bunifuMetroTextbox2.Name = "bunifuMetroTextbox2";
+            this.bunifuMetroTextbox2.Size = new System.Drawing.Size(395, 44);
+            this.bunifuMetroTextbox2.TabIndex = 4;
+            this.bunifuMetroTextbox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.bunifuMetroTextbox2.OnValueChanged += new System.EventHandler(this.bunifuMetroTextbox2_OnValueChanged);
+            // 
+            // bunifuCheckbox1
+            // 
+            this.bunifuCheckbox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(205)))), ((int)(((byte)(117)))));
+            this.bunifuCheckbox1.ChechedOffColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(135)))), ((int)(((byte)(140)))));
+            this.bunifuCheckbox1.Checked = true;
+            this.bunifuCheckbox1.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(205)))), ((int)(((byte)(117)))));
+            this.bunifuCheckbox1.ForeColor = System.Drawing.Color.White;
+            this.bunifuCheckbox1.Location = new System.Drawing.Point(181, 275);
+            this.bunifuCheckbox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bunifuCheckbox1.Name = "bunifuCheckbox1";
+            this.bunifuCheckbox1.Size = new System.Drawing.Size(20, 20);
+            this.bunifuCheckbox1.TabIndex = 5;
+            // 
+            // bunifuiOSSwitch1
+            // 
+            this.bunifuiOSSwitch1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuiOSSwitch1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuiOSSwitch1.BackgroundImage")));
+            this.bunifuiOSSwitch1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuiOSSwitch1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuiOSSwitch1.Location = new System.Drawing.Point(483, 304);
+            this.bunifuiOSSwitch1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bunifuiOSSwitch1.Name = "bunifuiOSSwitch1";
+            this.bunifuiOSSwitch1.OffColor = System.Drawing.Color.Gray;
+            this.bunifuiOSSwitch1.OnColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(202)))), ((int)(((byte)(94)))));
+            this.bunifuiOSSwitch1.Size = new System.Drawing.Size(35, 20);
+            this.bunifuiOSSwitch1.TabIndex = 6;
+            this.bunifuiOSSwitch1.Value = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 522);
+            this.Controls.Add(this.bunifuiOSSwitch1);
+            this.Controls.Add(this.bunifuCheckbox1);
+            this.Controls.Add(this.bunifuMetroTextbox2);
+            this.Controls.Add(this.bunifuMetroTextbox1);
+            this.Controls.Add(this.Txt_username);
+            this.Controls.Add(this.Btn_login);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuThinButton2 Btn_login;
+        private Bunifu.Framework.UI.BunifuMetroTextbox bunifuMetroTextbox2;
+        private Bunifu.Framework.UI.BunifuMetroTextbox bunifuMetroTextbox1;
+        private Bunifu.Framework.UI.BunifuMetroTextbox Txt_username;
+        private Bunifu.Framework.UI.BunifuiOSSwitch bunifuiOSSwitch1;
+        private Bunifu.Framework.UI.BunifuCheckbox bunifuCheckbox1;
+    }
+}
---
+++ taamol/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taamol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_login_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuMetroTextbox2_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
